81fb85e0bef1ff85212e51d74e839276825be86e
John Schreck <jsschreck@gmail.com>
2022-01-21 14:35:47 -0700

    Adding the prelimenary notebooks for gecko and surface layer data sets

---
+++ evidential/evidential.py
@@ -0,0 +1,42 @@
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class LinearNormalGamma(nn.Module):
+    def __init__(self, in_chanels, out_channels):
+        super().__init__()
+        self.linear = nn.Linear(in_chanels, out_channels*4)
+
+    def evidence(self, x):
+        return torch.log(torch.exp(x) + 1)
+
+    def forward(self, x):
+        pred = self.linear(x).view(x.shape[0], -1, 4)
+        mu, logv, logalpha, logbeta = [w.squeeze(-1) for w in torch.split(pred, 1, dim=-1)]
+        return mu, self.evidence(logv), self.evidence(logalpha) + 1, self.evidence(logbeta)
+
+
+def nig_nll(y, gamma, v, alpha, beta):
+    two_blambda = 2 * beta * (1 + v)
+    nll = 0.5 * torch.log(np.pi / v) \
+            - alpha * torch.log(two_blambda) \
+            + (alpha + 0.5) * torch.log(v * (y - gamma) ** 2 + two_blambda) \
+            + torch.lgamma(alpha) \
+            - torch.lgamma(alpha + 0.5)
+
+    return nll
+
+
+def nig_reg(y, gamma, v, alpha, beta):
+    error = F.l1_loss(y, gamma, reduction="none")
+    evi = 2 * v + alpha
+    return error * evi
+
+
+def evidential_regresssion_loss(y, pred, coeff=1.0):
+    gamma, v, alpha, beta = pred
+    loss_nll = nig_nll(y, gamma, v, alpha, beta)
+    loss_reg = nig_reg(y, gamma, v, alpha, beta)
+    return loss_nll.mean() + coeff * loss_reg.mean()
